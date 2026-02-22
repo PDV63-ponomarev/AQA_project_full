fd829ed95b0243c22f58681ec67065cde7d26338
Dima <graiwers@gmail.com>
2026-02-22 18:12:11 +0400

    changed test

--- tests/long/test_long.py
+++ tests/long/test_long.py
@@ -35,6 +35,7 @@ def test_form_ru(setup_browser):
             '01 Jan 2020'
         )
         time.sleep(2)
+        browser.element('#dateOfBirthInput').send_keys(Keys.ENTER)
         browser.element('#dateOfBirthInput').should(have.value('01 Jan 2020'))
 
         browser.element('#subjectsInput').type(predmet1).send_keys(Keys.ENTER)
